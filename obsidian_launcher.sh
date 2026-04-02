e646d9f9e8e5d9a7fdefdfc0b55ebb00c9f6aee2
Nunki1729 <cocosunusu@gmail.com>
2026-04-02 17:39:13 +0200

    Adding license

--- obsidian_launcher.sh
+++ obsidian_launcher.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Program: Obsidian file launcher
+# Copyright (C) 2026 Javier Santiago (Nunki1729)
+ 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+ 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # Si se hace click en el icono se abre directamente
 if [[ -z "$1" ]]; then
     __HOME__/.obsidian_app/Obsidian.AppImage & disown
